3604b50fe80563ad3a7fb5a8bae3e2d8dea5c358
Dirk Wright the 2nd <dirkwrightthe2nd@MacBook-Air-5.local>
2020-12-03 12:20:52 -0800

    First Commit

---
+++ .vscode/cquery_cached_index/@Users@dirkwrightthe2nd@Documents@P6project/include@functions.h
@@ -0,0 +1,4 @@
+#ifndef _FUNCTIONS_H_
+#define _FUNCTIONS_H_
+
+#include "main.h"
---
+++ .vscode/cquery_cached_index/@Users@dirkwrightthe2nd@Documents@P6project/src@opcontrol.c
@@ -0,0 +1,9 @@
+
+#include "main.h"
+
+void operatorControl() {
+	while (1) {
+		printf("Hello PROS User!\n");
+		delay(20);
+	}
+}

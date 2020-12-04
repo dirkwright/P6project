eef832e3f99ef7152fce38624f8d32b29976626a
mjcross8 <mekhicrossland@students.berkeely.net>
2020-12-04 13:23:15 -0800

    Added my name

---
+++ .vscode/cquery_cached_index/c@@users@admin@documents@p6project/src@opcontrol.c
@@ -0,0 +1,54 @@
+//include commands for all .h function files
+#include "main.h"
+#include "functions.h"
+/* - - - - - - - -  students - - - - - - - -
+Dirk Wright
+Nate Weitzel-King
+Reuben Lewis
+Ayush Shah
+James Knowles
+Aidan Alcala
+Jaden Jessup
+Alex San Martin
+Diego R
+Rohun B
+Mekhi C
+-- - - - - - - - - - - - - -  - - - - - - - - */
+
+
+/* - - - - - - - - - - - - - -  - - - - - - - - - -
+					port assigments used in this projet
+Motors
+	1 - Left Drive Motor
+	2 - claw motot
+	3 - writs motor
+	4 - elbow motor
+	5 - shoulder motor
+ports 6,7, 8, 9 not used
+	10 - Right Drive Motor
+
+Analogue Sensors
+	1 - Left line sensor
+	2 - Right line sensor
+	3 - center line sensor
+other ports not used
+
+Digital Sensors
+	1 - Ultrasinic out
+	2 - Ultrasonic in
+	3 - Elbow limit switch
+	4 - Shoulder Limit swiitch
+	5 - bumper switch
+	6 - Elbow Encoder
+	7 - Elbow Encoder
+	8 - Shoulder Encoder
+	9 - Shoulder Encoder
+	10
+- - - - - - - - - - - - - - - - - - - - - - - - - */
+
+void operatorControl() {
+	while (1) {
+		printf("Hellow P6 User\n");
+		delay(20);
+	}
+}

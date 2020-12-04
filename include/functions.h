2102103214ff4de67f48d9db078f78ea8e529301
Dirk Wright the 2nd <dirkwrightthe2nd@MacBook-Air-5.local>
2020-12-04 12:28:47 -0800

    added wave function to function.h

--- include/functions.h
+++ include/functions.h
@@ -2,3 +2,19 @@
 #define _FUNCTIONS_H_
 
 #include "main.h"
+
+
+void armWave(int numberOfWaves)
+{
+  int loop = 0;
+while (loop < numberOfWaves)
+{
+motorSet(5,100);
+delay(1000);
+motorSet(5,-100);
+delay(1000);
+loop ++;
+}
+motorSet(5,0);
+
+}

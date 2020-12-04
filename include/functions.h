02460158aa0bca39ae1f0792d73ad20b160011f2
Reuben-L <66336325+Reuben-L@users.noreply.github.com>
2020-12-04 12:32:18 -0800

    added spin function

--- include/functions.h
+++ include/functions.h
@@ -3,18 +3,31 @@
 
 #include "main.h"
 
-
 void armWave(int numberOfWaves)
 {
   int loop = 0;
-while (loop < numberOfWaves)
-{
-motorSet(5,100);
-delay(1000);
-motorSet(5,-100);
-delay(1000);
-loop ++;
+  while (loop < numberOfWaves)
+  {
+    motorSet(5, 100);
+    delay(1000);
+    motorSet(5, -100);
+    delay(1000);
+    loop++;
+  }
+  motorSet(5, 0);
 }
-motorSet(5,0);
-
+void spin(int numberOfSpins)
+{
+  int loop = 0;
+  while (loop < numberOfWaves)
+  {
+    motorSet(1, 100);
+    motorSet(10, 100);
+    delay(5000);
+    motorSet(1, 0);
+    motorSet(10, 0;
+    delay(5000)
+    loop++;
+  }
+  motorSet(5, 0);
 }

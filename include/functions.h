37ecf8d00c48f5e33d84708e61129311a665f8a6
Nate Weitzel-King <nathanielweitzel@students.berkeley.net>
2020-12-04 12:47:09 -0800

    Dance dance Robolution!!!

--- include/functions.h
+++ include/functions.h
@@ -18,3 +18,24 @@ loop ++;
 motorSet(5,0);
 
 }
+
+void roboDance(int danceTime){
+int loop = 0
+while (loop < danceTime)
+{
+motorSet(1, 500);
+delay(500);
+motorSet(10, 1000);
+delay(500);
+motorSet(1, 500);
+delay(500);
+motorSet(1, -200);
+motorSet(10, -200);
+delay(100);
+motorSet(1, 200);
+motorSet(10, 200);
+loop++;
+}
+motorSet(1, 0);
+motorSet(10, 0);
+}
